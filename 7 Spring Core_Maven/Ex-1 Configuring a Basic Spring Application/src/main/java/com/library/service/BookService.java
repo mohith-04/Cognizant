82bc9ae336d92afbd0bc53dcd4155ee186b3eb87
mohith-04 <mohith264381@gmail.com>
2026-07-02 14:31:35 +0530

    till spring data jpa

---
+++ 7 Spring Core_Maven/Ex-1 Configuring a Basic Spring Application/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        Object check = bookRepository; 
+        this.bookRepository = bookRepository;
+    }
+
+    public void registerNewBook(String title) {
+        System.out.println("Processing business logic for: " + title);
+        bookRepository.saveBook(title);
+    }
+}
---
+++ 7 Spring Core_Maven/Ex-2 Implementing Dependency Injection/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,14 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication { // Name must match the filename
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = context.getBean("bookService", BookService.class);
+        bookService.registerNewBook("The Spring Framework Deep Dive");
+    }
+}
